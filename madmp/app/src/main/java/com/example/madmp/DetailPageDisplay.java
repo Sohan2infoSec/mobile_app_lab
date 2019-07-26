b6d3597708a255341c8e395e2183b107bbcba098
Sang <sangnn@sohan-computer.lan>
2019-07-26 16:41:36 +0700

    Add Username, Placename and a funny Spinner

---
+++ madmp/app/src/main/java/com/example/madmp/DetailPageDisplay.java
@@ -0,0 +1,33 @@
+package com.example.madmp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DetailPageDisplay extends Activity {
+    private TextView username_view;
+    private TextView placename_view;
+    private TextView foodtype_view;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_display);
+
+        username_view = (TextView)findViewById(R.id.show_username);
+        placename_view = (TextView)findViewById(R.id.show_placename);
+        foodtype_view = (TextView)findViewById(R.id.show_foodtype);
+
+        Bundle extras = getIntent().getExtras();
+
+        if (extras != null) {
+            String username = extras.getString("username");
+            String placename = extras.getString("placename");
+            String foodtype = extras.getString("foodtype");
+
+            username_view.setText("Username: " + username);
+            placename_view.setText("Placename: " + placename);
+            foodtype_view.setText("Placename: " + foodtype);
+        }
+    }
+}
